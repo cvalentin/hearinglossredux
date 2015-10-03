7630e325a2b0bc09c7cd64df36f0352d56452359
Ngoc Do <ngocmdo@gmail.com>
2015-10-03 15:25:29 -0700

    add another section in home

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php require("header.php"); ?>
 
-<div id="home-carousel"class="image-carousel large-12 row">
-	<div><img src="http://placehold.it/1000x400&text=[ img 1 ]" /></div>
-	<div><img src="http://placehold.it/1000x400&text=[ img 2 ]" /></div>
-	<div><img src="http://placehold.it/1000x400&text=[ img 3 ]" /></div>
-</div>
+<section> 
+	<div id="home-carousel"class="image-carousel large-12 row">
+		<div><img src="http://placehold.it/1000x400&text=[ img 1 ]" /></div>
+		<div><img src="http://placehold.it/1000x400&text=[ img 2 ]" /></div>
+		<div><img src="http://placehold.it/1000x400&text=[ img 3 ]" /></div>
+	</div>
+</section>
+<section>
+	<div class="row large-12">
+		<p>The Global Foundation for Children With Hearing Loss aims to make a <span class="highlighted"> direct and lasting impact on the futures of thousands of deaf and hard of hearing children</span> around the world.</p>
+	</div>
+</section>
 
 <?php require("footer.php"); ?>
