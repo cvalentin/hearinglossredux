67d996dace1639ef7b91b0c43d88cb90698e8a77
Ngoc Do <ngocmdo@gmail.com>
2015-10-03 14:53:54 -0700

    add image carousel for home page

--- header.php
+++ header.php
@@ -11,36 +11,26 @@ $page_name = "Home";
     <head>
         <title><?php echo $page_name . " - " . $title; ?></title>
         <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-        
         <link rel="stylesheet" href="scripts/css/foundation.css">
-
       <!-- This is how you would link your custom stylesheet -->
       <link rel="stylesheet" href="scripts/css/app.css">
-
+      <link rel="stylesheet" href="scripts/slick/slick.css">
+      <link rel="stylesheet" href="scripts/slick/slick-theme.css">
       <script src="scripts/js/vendor/modernizr.js"></script>
-    
+      <script src="scripts/js/vendor/modernizr.js"></script>
     </head>
-    <body>           
+    <body>
         <nav class="top-bar" data-topbar>
           <ul class="title-area">
-             
             <li class="name">
-              <h1>
-                <a href="#">
-                  <?php echo $title ?>
-                </a>
-              </h1>
+              <h1><a href="#"><?php echo $title ?></a></h1>
             </li>
             <li class="toggle-topbar menu-icon"><a href="#"><span>menu</span></a></li>
           </ul>
-       
           <section class="top-bar-section">
-             
             <ul class="right">
               <li><a href="#">Main Item 1</a></li>
-              <li class="divider"></li>
               <li><a href="#">Main Item 2</a></li>
-              <li class="divider"></li>
               <li class="has-dropdown">
                 <a href="#">Main Item 3</a>
                 <ul class="dropdown">
