ea84023376b822719e230bb26b0cd4226f89c720
Ngoc Do <ngocmdo@gmail.com>
2015-10-03 16:05:05 -0700

    centered home tile

--- index.php
+++ index.php
@@ -1,15 +1,53 @@
 <?php require("header.php"); ?>
 
 <section> 
-	<div id="home-carousel"class="image-carousel large-12 row">
+	<div id="home-carousel"class="image-carousel row small-12">
 		<div><img src="http://placehold.it/1000x400&text=[ img 1 ]" /></div>
 		<div><img src="http://placehold.it/1000x400&text=[ img 2 ]" /></div>
 		<div><img src="http://placehold.it/1000x400&text=[ img 3 ]" /></div>
 	</div>
 </section>
 <section>
-	<div class="row large-12">
+	<div class="row small-12">
 		<p>The Global Foundation for Children With Hearing Loss aims to make a <span class="highlighted"> direct and lasting impact on the futures of thousands of deaf and hard of hearing children</span> around the world.</p>
+	</div>
+</section>
+
+<section>
+	<div class="row small-collapse large-uncollapse">
+		<div class="small-4 large-4 column">
+			<div class="home-tile">
+				<h2>Success Stories</h2>
+				<div class="row">
+					<div class="column small-centered">
+						<img src="http://placehold.it/250x250&text=[ img ]" />
+					</div>
+				</div>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tortor eros, dignissim non leo eget, consequat laoreet nibh. Donec quis tellus vel felis vulputate convallis. Curabitur sapien massa, tempus ac eros et, consequat ultrices velit. Quisque non dui hendrerit, volutpat enim hendrerit, congue dui. Cras vehicula nulla nisl, quis ullamcorper odio tincidunt eget. Vivamus mollis tristique sapien, eu laoreet sapien commodo at. Morbi congue convallis aliquam. Suspendisse commodo magna a bibendum laoreet. Mauris sit amet ligula in erat dignissim egestas.</p>
+			</div>
+		</div>
+		<div class="small-4 large-4 column">
+			<div class="home-tile">
+				<h2>Success Stories</h2>
+				<div class="row">
+					<div class="column small-centered">
+						<img src="http://placehold.it/250x250&text=[ img ]" />
+					</div>
+				</div>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tortor eros, dignissim non leo eget, consequat laoreet nibh. Donec quis tellus vel felis vulputate convallis. Curabitur sapien massa, tempus ac eros et, consequat ultrices velit. Quisque non dui hendrerit, volutpat enim hendrerit, congue dui. Cras vehicula nulla nisl, quis ullamcorper odio tincidunt eget. Vivamus mollis tristique sapien, eu laoreet sapien commodo at. Morbi congue convallis aliquam. Suspendisse commodo magna a bibendum laoreet. Mauris sit amet ligula in erat dignissim egestas.</p>
+			</div>
+		</div>
+		<div class="small-4 large-4 column">
+			<div class="home-tile">
+				<h2>Success Stories</h2>
+				<div class="row">
+					<div class="column small-centered">
+						<img src="http://placehold.it/250x250&text=[ img ]" />
+					</div>
+				</div>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tortor eros, dignissim non leo eget, consequat laoreet nibh. Donec quis tellus vel felis vulputate convallis. Curabitur sapien massa, tempus ac eros et, consequat ultrices velit. Quisque non dui hendrerit, volutpat enim hendrerit, congue dui. Cras vehicula nulla nisl, quis ullamcorper odio tincidunt eget. Vivamus mollis tristique sapien, eu laoreet sapien commodo at. Morbi congue convallis aliquam. Suspendisse commodo magna a bibendum laoreet. Mauris sit amet ligula in erat dignissim egestas.</p>
+			</div>
+		</div>
 	</div>
 </section>
 
