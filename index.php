5a05c95b289fe77159bd0985ab4968260a470191
Ngoc Do <ngocmdo@gmail.com>
2015-10-03 16:13:27 -0700

    larger mission statement, padding for home tiles

--- index.php
+++ index.php
@@ -8,16 +8,16 @@
 	</div>
 </section>
 <section>
-	<div class="row small-12">
+	<div class="mission-statement row small-12">
 		<p>The Global Foundation for Children With Hearing Loss aims to make a <span class="highlighted"> direct and lasting impact on the futures of thousands of deaf and hard of hearing children</span> around the world.</p>
 	</div>
 </section>
 
 <section>
-	<div class="row small-collapse large-uncollapse">
+	<div class="home-tile-container row small-collapse large-uncollapse">
 		<div class="small-4 large-4 column">
 			<div class="home-tile">
-				<h2>Success Stories</h2>
+				<h3>Success Stories</h3>
 				<div class="row">
 					<div class="column small-centered">
 						<img src="http://placehold.it/250x250&text=[ img ]" />
@@ -28,7 +28,7 @@
 		</div>
 		<div class="small-4 large-4 column">
 			<div class="home-tile">
-				<h2>Success Stories</h2>
+				<h3>Where We Work</h3>
 				<div class="row">
 					<div class="column small-centered">
 						<img src="http://placehold.it/250x250&text=[ img ]" />
@@ -39,7 +39,7 @@
 		</div>
 		<div class="small-4 large-4 column">
 			<div class="home-tile">
-				<h2>Success Stories</h2>
+				<h3>Get Involved</h3>
 				<div class="row">
 					<div class="column small-centered">
 						<img src="http://placehold.it/250x250&text=[ img ]" />
