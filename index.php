6d32241a6a83d0ef8f79e571583eb61d264f866a
Ngoc Do <ngocmdo@gmail.com>
2015-10-03 16:36:11 -0700

    full height home tiles

--- index.php
+++ index.php
@@ -8,44 +8,32 @@
 	</div>
 </section>
 <section>
-	<div class="mission-statement row small-12">
+	<div id="mission-statement" class="row small-12">
 		<p>The Global Foundation for Children With Hearing Loss aims to make a <span class="highlighted"> direct and lasting impact on the futures of thousands of deaf and hard of hearing children</span> around the world.</p>
 	</div>
 </section>
 
 <section>
-	<div class="home-tile-container row small-collapse large-uncollapse">
-		<div class="small-4 large-4 column">
+	<div class="home-tile-container row small-collapse large-uncollapse" data-equalizer>
+		<div class="small-12 medium-4 large-4 column" data-equalizer-watch>
 			<div class="home-tile">
 				<h3>Success Stories</h3>
-				<div class="row">
-					<div class="column small-centered">
-						<img src="http://placehold.it/250x250&text=[ img ]" />
-					</div>
-				</div>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tortor eros, dignissim non leo eget, consequat laoreet nibh. Donec quis tellus vel felis vulputate convallis. Curabitur sapien massa, tempus ac eros et, consequat ultrices velit. Quisque non dui hendrerit, volutpat enim hendrerit, congue dui. Cras vehicula nulla nisl, quis ullamcorper odio tincidunt eget. Vivamus mollis tristique sapien, eu laoreet sapien commodo at. Morbi congue convallis aliquam. Suspendisse commodo magna a bibendum laoreet. Mauris sit amet ligula in erat dignissim egestas.</p>
+				<img src="http://placehold.it/250x250&text=[ img ]" />
+				<p>Successfully impacting children all over the world. Find out about <a href="">Celina's story</a> and what we've done to help her.</p>
 			</div>
 		</div>
-		<div class="small-4 large-4 column">
+		<div class="small-12 medium-4 large-4 column" data-equalizer-watch>
 			<div class="home-tile">
 				<h3>Where We Work</h3>
-				<div class="row">
-					<div class="column small-centered">
-						<img src="http://placehold.it/250x250&text=[ img ]" />
-					</div>
-				</div>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tortor eros, dignissim non leo eget, consequat laoreet nibh. Donec quis tellus vel felis vulputate convallis. Curabitur sapien massa, tempus ac eros et, consequat ultrices velit. Quisque non dui hendrerit, volutpat enim hendrerit, congue dui. Cras vehicula nulla nisl, quis ullamcorper odio tincidunt eget. Vivamus mollis tristique sapien, eu laoreet sapien commodo at. Morbi congue convallis aliquam. Suspendisse commodo magna a bibendum laoreet. Mauris sit amet ligula in erat dignissim egestas.</p>
+				<img src="http://placehold.it/250x250&text=[ img ]" />
+				<p>GFCWHL operates in many regions all around teh globe, and we are expanding our reach every day! <a href="">Visit our blog</a> to keep up with the most recent news.</p>
 			</div>
 		</div>
-		<div class="small-4 large-4 column">
+		<div class="small-12 medium-4 large-4 column" data-equalizer-watch>
 			<div class="home-tile">
 				<h3>Get Involved</h3>
-				<div class="row">
-					<div class="column small-centered">
-						<img src="http://placehold.it/250x250&text=[ img ]" />
-					</div>
-				</div>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tortor eros, dignissim non leo eget, consequat laoreet nibh. Donec quis tellus vel felis vulputate convallis. Curabitur sapien massa, tempus ac eros et, consequat ultrices velit. Quisque non dui hendrerit, volutpat enim hendrerit, congue dui. Cras vehicula nulla nisl, quis ullamcorper odio tincidunt eget. Vivamus mollis tristique sapien, eu laoreet sapien commodo at. Morbi congue convallis aliquam. Suspendisse commodo magna a bibendum laoreet. Mauris sit amet ligula in erat dignissim egestas.</p>
+				<img src="http://placehold.it/250x250&text=[ img ]" />
+				<p>We welcome credentiated professionals to join our team. <a href="">Find out more here.</a> Together, we can make a difference, today.</p>
 			</div>
 		</div>
 	</div>
