c0fce141b65464cf01f9676426825fcac2515beb
Carlo Valentin <me@carlovalentin.com>
2015-10-03 14:22:14 -0700

    lolwut

---
+++ locations.php
@@ -0,0 +1,16 @@
+<?php require("header.php"); ?>
+
+<div class="row"> 
+	<div class = "large-12 columns location-header">
+		<h3 class = "text-center"> 
+			The Global Foundation for Children With Hearing Loss aims to make a direct and lasting impact on the futures of thousands of deaf and hard of hearing children around the world.
+		</h3>
+	</div>
+	<div class = "large-12 columns">
+		<hr>
+	</div>
+
+</div>
+
+
+<?php require("footer.php"); ?>
