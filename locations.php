920ade37d7ea5f680dfe7b24170665c77bd767c5
Carlo Valentin <me@carlovalentin.com>
2015-10-03 14:56:19 -0700

    Locations Page

--- locations.php
+++ locations.php
@@ -1,15 +1,65 @@
 <?php require("header.php"); ?>
 
 <div class="row"> 
-	<div class = "large-12 columns location-header">
+	<div class = "small-11 small-centered columns">
 		<h3 class = "text-center"> 
 			The Global Foundation for Children With Hearing Loss aims to make a direct and lasting impact on the futures of thousands of deaf and hard of hearing children around the world.
 		</h3>
 	</div>
-	<div class = "large-12 columns">
+
+	<div class = "row">
 		<hr>
+		<div class = "loc-element small-12">
+			<div class = "small-2 columns">
+				<!-- Thumbnail -->
+				<a class="th loc-thumb" href="#">
+	  				<img src="../images/thumb.jpg">
+				</a>
+			</div>
+			<div class = "small-10 columns loc-content">
+				<h4>This is a title.</h4>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer accumsan semper velit, eu dictum ligula. Nunc gravida diam at erat euismod molestie. Proin ornare lorem ut magna volutpat, vestibulum facilisis mi condimentum. Etiam imperdiet ante et leo suscipit faucibus.
+				</p>
+			</div>
+		</div>
 	</div>
 
+	<div class = "row">
+		<hr>
+		<div class = "loc-element small-12">
+			<div class = "small-2 columns">
+				<!-- Thumbnail -->
+				<a class="th loc-thumb" href="#">
+	  				<img src="../images/thumb.jpg">
+				</a>
+			</div>
+			<div class = "small-10 columns loc-content">
+				<h4>This is a title.</h4>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer accumsan semper velit, eu dictum ligula. Nunc gravida diam at erat euismod molestie. Proin ornare lorem ut magna volutpat, vestibulum facilisis mi condimentum. Etiam imperdiet ante et leo suscipit faucibus.
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<div class = "row">
+		<hr>
+		<div class = "loc-element small-12">
+			<div class = "small-2 columns">
+				<!-- Thumbnail -->
+				<a class="th loc-thumb" href="#">
+	  				<img src="../images/thumb.jpg">
+				</a>
+			</div>
+			<div class = "small-10 columns loc-content">
+				<h4>This is a title.</h4>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer accumsan semper velit, eu dictum ligula. Nunc gravida diam at erat euismod molestie. Proin ornare lorem ut magna volutpat, vestibulum facilisis mi condimentum. Etiam imperdiet ante et leo suscipit faucibus.
+				</p>
+			</div>
+		</div>
+	</div>
 </div>
 
 
