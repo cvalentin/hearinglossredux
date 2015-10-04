bcbeaa4df905863404faa01f2c551dd252a13bfa
ramirs <ramirs2015@gmail.com>
2015-10-04 11:39:10 -0700

    footer formatting / content additions

--- footer.php
+++ footer.php
@@ -1,12 +1,29 @@
-        <footer class="row full-width footer">
+        <!--<footer class="row full-width footer">
           <div class="small-12 large-5 large-offset-1 column"> Contact Us: <a href="mailto:info@childrenwithhearingloss.org">info@childrenwithhearingloss.org</a> <a href="">(206) 618-4201</a></div>
           <div class="small-12 large-6 column">
             <p>The Global Foundation for Children with Hearing Loss is a 501(c)3 Organization established in 2009.</p>
           </div>
         </footer>
+        -->
+        <footer class="row">
+          <div class = "row">
+            <hr>
+            <div class = "small-12 small-centered">
+            </div>
+          </div>
+          <div class="small-12 large-12 small-centered columns">
+            <div>
+              <div><i class="fa fa-phone"></i>(206) 618-4201</div>
+              <div><i class="fa fa-envelope"></i><a href="mailto:info@childrenwithhearingloss.org">info@childrenwithhearingloss.org</a></div>
+              <p>The Global Foundation for Children with Hearing Loss is a 501(c)3 Organization established in 2009.</p>
+            </div>
+          </div>
+        </footer>
+
         <script type="text/javascript" src="scripts/js/vendor/jquery.js"></script>
         <script type="text/javascript" src="scripts/js/foundation.min.js"></script>
         <script type="text/javascript" src="scripts/slick/slick.min.js"></script>
         <script type="text/javascript" src="scripts/js/main.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     </body>
 </html>
