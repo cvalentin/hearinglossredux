eec14d91120aeb78c12dfade215158185248f3d3
Carlo Valentin <me@carlovalentin.com>
2015-10-04 16:07:15 -0700

    lol

--- header.php
+++ header.php
@@ -25,7 +25,7 @@ $page_name = "Home";
       <nav class="top-bar" data-topbar role="navigation">
         <ul class="title-area">
           <li class="name">
-            <a href="/index.php">
+            <a href="index.php">
             <img src="images/symbol.png" id="logo" class="show-for-medium-up">
             <h1 id="logo_name" class="text-medium">Global Foundation for Children With Hearing Loss</h1>
             </a>
