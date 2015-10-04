29f43945854d26eb3805a959e3b4b291e56e0c5d
Ngoc Do <ngocmdo@gmail.com>
2015-10-04 13:19:48 -0700

    change carousel back to slick

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
-	$(document).foundation({
-		orbit: {
-			slide_number: false,
-			bullets: false
-		}
-	});
+	// $(document).foundation({
+	// 	orbit: {
+	// 		slide_number: false,
+	// 		bullets: false
+	// 	}
+	// });
+	
+	$(document).foundation();
+
+	$('.image-carousel').slick({
+		dots: true,
+		mobileFirst: true
+	})
 });
