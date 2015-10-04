d80871a73e1df2e63350faea0437bc5567f7c8bf
ramirs <ramirs2015@gmail.com>
2015-10-04 12:17:05 -0700

    styling updates to index, looks super purdy now

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php require("header.php"); ?>
 
 
-<div class="row">
-	<ul class="example-orbit" data-orbit>
-  <li class="active"><img src="images/home/home-1.jpg" alt="slide 1" /></li>
-  <li><img src="images/home/home-2.jpg" alt="slide 2" /></li>
-  <li><img src="images/home/home-3.jpg" alt="slide 3" /></li>
-  <li><img src="images/home/home-4.jpg" alt="slide 4" /></li>
-</ul>
-	
+<div class="headline-section">
+	<div class="row">
+		<ul class="example-orbit" data-orbit>
+	  <li class="active"><img src="images/home/home-1.jpg" alt="slide 1" /></li>
+	  <li><img src="images/home/home-2.jpg" alt="slide 2" /></li>
+	  <li><img src="images/home/home-3.jpg" alt="slide 3" /></li>
+	  <li><img src="images/home/home-4.jpg" alt="slide 4" /></li>
+	</ul>
+		
+	</div>
 </div>
-
-<hr>
 
 <div class="row">
 	<div id="mission-statement-large" class="large-12 show-for-large-only">
@@ -22,32 +22,33 @@
 	</div>
 </div>
 
-<hr>
-
-<div class="row home-tile-container">
-	<div class="small-collapse large-uncollapse" data-equalizer>
-		<div class="small-12 medium-4 large-4 column" data-equalizer-watch>
-			<div class="home-tile">
-				<h3>Success Stories</h3>
-				<img src="http://placehold.it/250x250&text=[ img ]" />
-				<p>Successfully impacting children all over the world. Find out about <a href="">Celina's story</a> and what we've done to help her.</p>
+<div class="card-section">
+	<div class="row home-tile-container">
+		<div class="small-collapse large-uncollapse" data-equalizer>
+			<div class="small-12 medium-4 large-4 column" data-equalizer-watch>
+				<div class="home-tile">
+					<h3>Success Stories</h3>
+					<img src="http://placehold.it/250x250&text=[ img ]" />
+					<p>Successfully impacting children all over the world. Find out about <a href="">Celina's story</a> and what we've done to help her.</p>
+				</div>
 			</div>
-		</div>
-		<div class="small-12 medium-4 large-4 column" data-equalizer-watch>
-			<div class="home-tile">
-				<h3>Where We Work</h3>
-				<img src="http://placehold.it/250x250&text=[ img ]" />
-				<p>GFCWHL operates in many regions all around teh globe, and we are expanding our reach every day! <a href="">Visit our blog</a> to keep up with the most recent news.</p>
+			<div class="small-12 medium-4 large-4 column" data-equalizer-watch>
+				<div class="home-tile">
+					<h3>Where We Work</h3>
+					<img src="http://placehold.it/250x250&text=[ img ]" />
+					<p>GFCWHL operates in many regions all around teh globe, and we are expanding our reach every day! <a href="">Visit our blog</a> to keep up with the most recent news.</p>
+				</div>
 			</div>
-		</div>
-		<div class="small-12 medium-4 large-4 column" data-equalizer-watch>
-			<div class="home-tile">
-				<h3>Get Involved</h3>
-				<img src="http://placehold.it/250x250&text=[ img ]" />
-				<p>We welcome credentiated professionals to join our team. <a href="">Find out more here.</a> Together, we can make a difference, today.</p>
+			<div class="small-12 medium-4 large-4 column" data-equalizer-watch>
+				<div class="home-tile">
+					<h3>Get Involved</h3>
+					<img src="http://placehold.it/250x250&text=[ img ]" />
+					<p>We welcome credentiated professionals to join our team. <a href="">Find out more here.</a> Together, we can make a difference, today.</p>
+				</div>
 			</div>
 		</div>
 	</div>
 </div>
+
 
 <?php require("footer.php"); ?>
