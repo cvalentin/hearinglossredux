8c1d8fb7fd36971db7282720f713e6aaa10e60fa
ramirs <ramirs2015@gmail.com>
2015-10-04 14:14:51 -0700

    added media links / titles

--- media.php
+++ media.php
@@ -2,18 +2,26 @@
 <div class="row media-container">
 	<div class="small-10 small-centered columns">
 		<ul class="small-block-grid-2 large-block-grid-3">
-			<li><img class = "th" src="images/video-placeholder.jpg"</li>
-			<li><img class = "th" src="images/video-placeholder.jpg"</li>
-			<li><img class = "th" src="images/video-placeholder.jpg"</li>
-			<li><img class = "th" src="images/video-placeholder.jpg"</li>
-			<li><img class = "th" src="images/video-placeholder.jpg"</li>
-			<li><img class = "th" src="images/video-placeholder.jpg"</li>
-			<li><img class = "th" src="images/video-placeholder.jpg"</li>
-			<li><img class = "th" src="images/video-placeholder.jpg"</li>
-			<li><img class = "th" src="images/video-placeholder.jpg"</li>
-			<li><img class = "th" src="images/video-placeholder.jpg"</li>
-			<li><img class = "th" src="images/video-placeholder.jpg"</li>
-			<li><img class = "th" src="images/video-placeholder.jpg"</li>
+			<li>
+				<a href="https://animoto.com/play/gPJVUm5BVxiePD5zVUc2gw">
+					<img class = "th" src="images/video-placeholder.jpg"  title="What If? (WHS)">
+				</a>
+			</li>
+			<li>
+				<a href="https://www.youtube.com/watch?v=49Sryq7wAsE">
+					<img class = "th" src="images/video-placeholder.jpg" title="Talk Vietnam w/ Paige Stringer">				
+				</a>
+			</li>
+			<li><img class = "th" src="images/video-placeholder.jpg"></li>
+			<li><img class = "th" src="images/video-placeholder.jpg"></li>
+			<li><img class = "th" src="images/video-placeholder.jpg"></li>
+			<li><img class = "th" src="images/video-placeholder.jpg"></li>
+			<li><img class = "th" src="images/video-placeholder.jpg"></li>
+			<li><img class = "th" src="images/video-placeholder.jpg"></li>
+			<li><img class = "th" src="images/video-placeholder.jpg"></li>
+			<li><img class = "th" src="images/video-placeholder.jpg"></li>
+			<li><img class = "th" src="images/video-placeholder.jpg"></li>
+			<li><img class = "th" src="images/video-placeholder.jpg"></li>
 		</ul>
 	</div>
 </div>
