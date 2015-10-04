ea8e547e6fe8ca3ec13af9061859310d0b50acea
Ngoc Do <ngocmdo@gmail.com>
2015-10-04 14:18:28 -0700

    location location location

--- locations.php
+++ locations.php
@@ -11,19 +11,18 @@
 </div>
 
 <div class="row">
-	<div class = "large-2 small-12 small-centered large-uncentered columns"><img src="images/locations/vietnam.jpg"></div>
-	<div class = "large-10 small-12 columns loc-content">
+	<div class="large-2 small-12 small-centered large-uncentered columns loc-thumb"><img src="images/locations/vietnam.jpg"></div>
+	<div class="large-10 small-12 columns loc-content">
 		<h4>Vietnam Deaf Education Program</h4>
 		<p>
 			Talk Vietnam, a national Vietnamese television show, shed light on pediatric hearing loss in Vietnam and what the Global Foundation and the Vietnamese deaf education and health care communities are doing to help support children with hearing loss.
 		</p>
 	</div>
 </div>
-<hr>
-
+<!-- 
 <div class="row">	
-	<div class = "large-2 small-12 small-centered large-uncentered columns"><img src="images/locations/vietnam2.jpg"></div>
-	<div class = "large-10 small-12 columns loc-content">
+	<div class="large-2 small-12 small-centered large-uncentered columns"><img src="images/locations/vietnam2.jpg"></div>
+	<div class="large-10 small-12 columns loc-content">
 		<h2>This is a title.</h2>
 		<p>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer accumsan semper velit, eu dictum ligula. Nunc gravida diam at erat euismod molestie. Proin ornare lorem ut magna volutpat, vestibulum facilisis mi condimentum. Etiam imperdiet ante et leo suscipit faucibus.
@@ -31,14 +30,14 @@
 	</div>
 </div>
 <hr>
-<div class = "row">
-	<div class = "large-2 small-12 small-centered large-uncentered columns"><img src="images/locations/equador.jpg"></div>
-	<div class = "large-10 small-12 columns loc-content">
+<div class="row">
+	<div class="large-2 small-12 small-centered large-uncentered columns"><img src="images/locations/equador.jpg"></div>
+	<div class="large-10 small-12 columns loc-content">
 		<h2>This is a title.</h2>
 		<p>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer accumsan semper velit, eu dictum ligula. Nunc gravida diam at erat euismod molestie. Proin ornare lorem ut magna volutpat, vestibulum facilisis mi condimentum. Etiam imperdiet ante et leo suscipit faucibus.
 		</p>
 	</div>
 </div>
-
+ -->
 <?php require("footer.php"); ?>
